dff4e818b45d02967db9245bf841ec51d0e9c27b
James Chang <jamesbchang@gmail.com>
2023-11-16 13:51:42 +0900

    update

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const fs = require('fs');
 const express = require('express');
 var cors = require('cors');
+const moment = require( 'moment');
+const listJson = require('./list.json');
 
 const bodyParser = require("body-parser");
 
@@ -15,35 +17,48 @@ const Utils = new ClientUtils();
 const PORT = process.env.PORT || 3111;
 const INDEX = '/index.html';
 
+var statusJson = {};
 
 const server = express()
 .use(cors())
 .use(bodyParser.urlencoded({ extended: false }))
 .use(bodyParser.json())
-.get('/', (req, res) => {
-	res.send('MV VMMMC - Client Registration server started !!!');
-})
+.get('/', (req, res) => {  res.send('MV VMMMC - Client Registration server started !!!');  })
+.get('/status', (req, res) => { res.send( { status: statusJson } ); })
 .post("/search", (req, res) => {
-	
-	// fs.readFile('list.json', 'utf8', (err, data) => {
-	// 	if (err) {
-	// 		console.error(err);
-	// 		return;
-	// 	}
-		
-	// 	Utils.checkTEIs(JSON.parse(data), function(response){
-	// 		res.send(response);
-	// 	});
-	// });
 
-	//Utils.checkTEIs(function(response){
-	//	res.send(response);
-	//});
+	/*
+	fs.readFile('list.json', 'utf8', (err, data) => {
+		if (err) {
+			console.error(err);
+			res.send( { msg: 'Error on file read - ' + err } );
+			return;
+		}
+		else
+		{
+	*/
+	statusJson = {};
 
+	var inputData = [];
+	if ( listJson && listJson.length > 0 ) inputData.push( listJson[0] );
 
-	Utils.checkTEIs2( ).then( itemList => {
-		res.send( { itemList: itemList } );
-	});
+	console.log( inputData );
+	console.log( JSON.stringify( inputData ) );
+
+	res.send( { msg: 'requestMade' } );
+
+	Utils.checkTEIs2( inputData, statusJson ).then( itemList => {
+
+		statusJson.itemList = itemList;
+
+		fs.writeFile( 'listOut_' + moment().format("YY-MM-DD_HHmmss") + '.json', JSON.stringify( itemList ), (error) => {
+			if (error) {
+			  console.log('An error has occurred ', error);
+			  return;
+			}
+			console.log('Data written successfully to disk');
+		 });
+	});		
 	
 })
 .listen(PORT, () => console.log(`Listening on ${PORT}` ));
--- utils/ClientUtils.js
+++ utils/ClientUtils.js
@@ -1,111 +1,93 @@
-  
-const {RequestUtils} = require("./RequestUtils");
+
+const { RequestUtils } = require("./RequestUtils");
 const RESTUtils = new RequestUtils();
 
 
 class ClientUtils {
 
-	constructor() { 
-        this.total = 0;
-        this.processingIdx = 0;
-        this.fullNameAttributeId = "LoGHwYUQZ9y";
-        this.fullNameMatchNo = 0;
-        this.firstNameMatchNo = 0;
-        this.lastNameMatchNo = 0;
+	constructor() 
+	{
+		this.total = 0;
+		this.processingIdx = 0;
+		this.fullNameAttributeId = "LoGHwYUQZ9y";
+		this.fullNameMatchNo = 0;
+		this.firstNameMatchNo = 0;
+		this.lastNameMatchNo = 0;
 
-        this.data = [
-            {
-              "Name": "William Thomas",
-              "First Name": "William",
-              "Last Name": "Thomas"
-            },
-            {
-              "Name": "Tarsizio Clement",
-              "First Name": "Tarsizio",
-              "Last Name": "Clement"
-            }
-        ];
-    };
+		// this.data = [ { "Name": "William Thomas",..
+	};
 
-    checkTEIs2 = async function() {
-       
-        var me = this;
-        var itemList = this.data;
-        
-        for ( var i = 0; i < itemList.length; i++ )
-        {
-            var item = itemList[i];
-            item.result = {};
+	checkTEIs2 = async function (inputData, statusJson) {
 
-            try
-            {
-                var searchedFullNameList = await me.checkTeiByName( item, "Name" );
-                item.result[ "Name" ] = me.resolveItem("Name", searchedFullNameList);
-    
-                if ( item.result[ "Name" ].length == 0 )
-                {
-                    var searchedFirstNameList = await me.checkTeiByName( item, "First Name" );
-                    item.result[ "First Name" ] = me.resolveItem("First Name", searchedFirstNameList);
-        
-                    var searchedLastNameList = await me.checkTeiByName( item, "Last Name" );
-                    item.result[ "Last Name" ]  = me.resolveItem("Last Name", searchedLastNameList);    
-                }
-            }
-            catch ( errMsg ) { }
-        }
+		var me = this;
+		var itemList = inputData; // this.data;
 
-        itemList.push({ perfectMatch: me.fullNameMatchNo, firstNameMatch: me.firstNameMatchNo, lastNameMatch: me.lastNameMatchNo});
+		for (var i = 0; i < itemList.length; i++) {
+			statusJson.processingIdx = i;
 
-        return itemList;
-    }
+			var item = itemList[i];
+			item.result = {};
 
-    resolveItem = function( prop, searchedList )
-    {
-        var me = this;
-        var result = [];
- 
-        if ( searchedList && searchedList.length > 0 )
-        {
-            for( var i=0; i<searchedList.length; i++)
-            {
-                try
-                {
-                    var item = searchedList[i];
-                    var fullNameAttrVal = item.attributes.find( attrVal => attrVal.attribute == me.fullNameAttributeId );
-                    if ( fullNameAttrVal ) result.push( fullNameAttrVal.value + "(" + ( item.trackedEntityInstance ) + ")" );    
-                }
-                catch ( errMsg ) { console.log( 'ERROR in searchedList, ' + errMsg ); }
-            }
-    
-            if( prop == "Name") me.fullNameMatchNo++;
-            else if( prop == "First Name") me.firstNameMatchNo++;
-            else if( prop == "Last Name") me.lastNameMatchNo++;   
-        }
+			try {
+				var searchedFullNameList = await me.checkTeiByName(item, "Name");
+				item.result["Name"] = me.resolveItem("Name", searchedFullNameList);
 
-        return result;
-    }
+				if (item.result["Name"].length == 0) {
+					var searchedFirstNameList = await me.checkTeiByName(item, "First Name");
+					item.result["First Name"] = me.resolveItem("First Name", searchedFirstNameList);
 
-    checkTeiByName = async function( item, nameProp )
-    {
-        var result = [];
+					var searchedLastNameList = await me.checkTeiByName(item, "Last Name");
+					item.result["Last Name"] = me.resolveItem("Last Name", searchedLastNameList);
+				}
+			}
+			catch (errMsg) { }
+		}
 
-        try
-        {
-            var filter = ":LIKE:" + item[ nameProp ];
-            var data = await RESTUtils.sendGetRequestAS( filter );
-            result = data.trackedEntityInstances;    
-        }
-        catch ( errMsg ) { 
-            item.error = "ERROR in searching name: " + item[ nameProp ] + ", item FullName: " + item.Name;
-            console.log( 'ERROR in checkTeiByName, ' + errMsg ); 
-        }
+		itemList.push({ perfectMatch: me.fullNameMatchNo, firstNameMatch: me.firstNameMatchNo, lastNameMatch: me.lastNameMatchNo });
 
-        return result;
-    }
+		return itemList;
+	}
+
+	resolveItem = function (prop, searchedList) {
+		var me = this;
+		var result = [];
+
+		if (searchedList && searchedList.length > 0) {
+			for (var i = 0; i < searchedList.length; i++) {
+				try {
+					var item = searchedList[i];
+					var fullNameAttrVal = item.attributes.find(attrVal => attrVal.attribute == me.fullNameAttributeId);
+					if (fullNameAttrVal) result.push(fullNameAttrVal.value + "(" + (item.trackedEntityInstance) + ")");
+				}
+				catch (errMsg) { console.log('ERROR in searchedList, ' + errMsg); }
+			}
+
+			if (prop == "Name") me.fullNameMatchNo++;
+			else if (prop == "First Name") me.firstNameMatchNo++;
+			else if (prop == "Last Name") me.lastNameMatchNo++;
+		}
+
+		return result;
+	}
+
+	checkTeiByName = async function (item, nameProp) {
+		var result = [];
+
+		try {
+			var filter = ":LIKE:" + item[nameProp];
+			var data = await RESTUtils.sendGetRequestAS(filter);
+			result = data.trackedEntityInstances;
+		}
+		catch (errMsg) {
+			item.error = "ERROR in searching name: " + item[nameProp] + ", item FullName: " + item.Name;
+			console.log('ERROR in checkTeiByName, ' + errMsg);
+		}
+
+		return result;
+	}
 
 }
 
 module.exports = {
-    ClientUtils
+	ClientUtils
 };
-  
